b90a8ec2ee9d463f0a6d26e4c760aae620f933a4
adsap <addesaputra11@gmail.com>
2021-06-15 20:50:54 +0700

    c5

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,12 +11,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
@@ -24,6 +24,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') next({ name: 'Login' })
+  else if (to.name !== 'Login' && !localStorage.getItem('access_token')) next({ name: 'Login' })
+  else if (to.name === 'Login' && localStorage.getItem('access_token')) next({ name: 'Home' })
+  else next()
 })
 
 export default router
---
+++ client/src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login-container">
+    <div class="left-login-page">
+      <h3>Please Login First</h3>
+      <form @submit.prevent="login">
+        <input
+          class="input-form"
+          type="email"
+          placeholder="your email here"
+          v-model="email"
+          required
+        />
+        <input
+          class="input-form"
+          type="password"
+          placeholder="your password here"
+          v-model="password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+    <div class="right-login-page">
+      <h1>Welcome to Animal Lovers</h1>
+      <img src="../assets/login.png" alt="login image" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$router.dispatch('login', { email: this.email, password: this.password })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
